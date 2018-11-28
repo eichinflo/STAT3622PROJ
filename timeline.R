1e3ddcd092b5ef367a2557fff9bb89b4d8b57aa5
Florian Eichin <eichinflo@aol.com>
2018-11-28 13:06:15 +0800

    Bugfix

--- timeline.R
+++ timeline.R
@@ -39,10 +39,20 @@ make_freq_plot = function(words,
     for (word in words) {
       data = rbind(data, data.frame(month = c(substr(as.vector(colnames(counts))[1:38], 1, 7), "2017-10"), 
                                     count = as.vector(counts_by_month_as_matrix[word,]), wrd = word))
-      data_trump = rbind(data_trump, data.frame(month = c(substr(as.vector(colnames(counts))[1:38], 1, 7), "2017-10"), 
-                                    count = as.vector(counts_by_month_as_matrix_trump[word,]), wrd = word))
-      data_clinton = rbind(data_clinton, data.frame(month = c(substr(as.vector(colnames(counts))[1:38], 1, 7), "2017-10"), 
-                                    count = as.vector(counts_by_month_as_matrix_clinton[word,]), wrd = word))
+      if (word %in% rownames(counts_by_month_as_matrix_trump)) {
+        data_trump = rbind(data_trump, data.frame(month = c(substr(as.vector(colnames(counts))[1:38], 1, 7), "2017-10"), 
+                                      count = as.vector(counts_by_month_as_matrix_trump[word,]), wrd = word))
+      } else {
+        data_trump = rbind(data_trump, data.frame(month = c(substr(as.vector(colnames(counts))[1:38], 1, 7), "2017-10"), 
+                                                  count = numeric(39), wrd = word))
+      }
+      if (word %in% rownames(counts_by_month_as_matrix_trump)) {
+        data_clinton = rbind(data_clinton, data.frame(month = c(substr(as.vector(colnames(counts))[1:38], 1, 7), "2017-10"), 
+                                      count = as.vector(counts_by_month_as_matrix_clinton[word,]), wrd = word))
+      } else {
+        data_clinton = rbind(data_clinton, data.frame(month = c(substr(as.vector(colnames(counts))[1:38], 1, 7), "2017-10"), 
+                                                      count = numeric(39), wrd = word))
+      }
     }
   } else {
     # frequency plot
@@ -106,7 +116,7 @@ make_freq_plot = function(words,
       theme_minimal() + theme(axis.text.x = element_text(colour="grey20",size=12, angle = 90, hjust = 1), 
                               axis.text.y = element_text(colour="grey20",size=12),  
                               axis.title.x = element_text(colour="grey20",size=17,  margin=margin(t=20)),
-                              axis.title.y = element_text(colour="grey20",size=17, margin=margin(r=20),
+                              axis.title.y = element_text(colour="grey20",size=17, margin=margin(r=20)),
                               legend.background = element_rect(fill="white", 
                                                                size=0.2, linetype = "blank"),
                               legend.title = element_blank(),
